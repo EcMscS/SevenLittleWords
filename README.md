2dfd15771d7f9b9c314d02b4560c79e3a3a57d13
Jeffrey Lai <jefflai@gmail.com>
2019-11-08 22:28:03 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SevenLittleWords
+ Hacking with Swift ; Project 8 ; Word game called 7 Little Words on the iPad
---
+++ SevenLittleWords/Controllers/MainVC.swift
@@ -0,0 +1,31 @@
+//
+//  MainVC.swift
+//  SevenLittleWords
+//
+//  Created by Jeffrey Lai on 11/8/19.
+//  Copyright © 2019 Jeffrey Lai. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemTeal
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
