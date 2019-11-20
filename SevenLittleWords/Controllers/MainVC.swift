b854603df7887c649ae00d9218b8d6214e8271e6
Jeffrey Lai <jefflai@gmail.com>
2019-11-19 22:51:51 -0800

    Finished Challenges
    Added extra level3.txt and finished 3 challenges

--- SevenLittleWords/Controllers/MainVC.swift
+++ SevenLittleWords/Controllers/MainVC.swift
@@ -5,12 +5,18 @@
 //  Created by Jeffrey Lai on 11/8/19.
 //  Copyright © 2019 Jeffrey Lai. All rights reserved.
 //
+//One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what’s going on:
+//1) Use the techniques you learned in project 2 to draw a thin gray line around the buttons view, to make it stand out from the rest of the UI.
+//2) If the user enters an incorrect guess, show an alert telling them they are wrong. You’ll need to extend the submitTapped() method so that if firstIndex(of:) failed to find the guess you show the alert.
+//3) Try making the game also deduct points if the player makes an incorrect guess. Think about how you can move to the next level – we can’t use a simple division remainder on the player’s score any more, because they might have lost some points.
+
 
 import UIKit
 
 class MainVC: UIViewController {
 
     //Variables
+    var clear: UIButton!
     var cluesLabel: UILabel!
     var answerLabel: UILabel!
     var currentAnswer: UITextField!
@@ -19,8 +25,13 @@ class MainVC: UIViewController {
     
     var activatedButtons = [UIButton]()
     var solutions = [String]()
+    var completedAnswers: Int = 0
     
-    var score = 0
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     var level = 1
     
     override func loadView() {
@@ -114,7 +125,7 @@ class MainVC: UIViewController {
         submit.setTitle("SUBMIT", for: .normal)
         view.addSubview(submit)
         
-        let clear = UIButton(type: .system)
+        clear = UIButton(type: .system)
         clear.translatesAutoresizingMaskIntoConstraints = false
         clear.setTitle("CLEAR", for: .normal)
         view.addSubview(clear)
@@ -152,6 +163,8 @@ class MainVC: UIViewController {
             for col in 0..<5 {
                 let letterButton = UIButton(type: .system)
                 letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+                letterButton.layer.borderWidth = 1
+                letterButton.layer.borderColor = UIColor.gray.cgColor
                 
                 letterButton.setTitle("WWW", for: .normal)
                 
@@ -216,17 +229,66 @@ class MainVC: UIViewController {
             }
         }
     }
+    
+    func levelUp(action: UIAlertAction) {
+        level += 1
+        solutions.removeAll(keepingCapacity: true)
+        
+        loadLevel()
+        
+        for btn in letterButtons {
+            btn.isHidden = false
+        }
+    }
 
     @objc func letterTapped(_ sender: UIButton) {
-        
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
     }
     
     @objc func submitTapped(_ sender: UIButton) {
+        guard let answerText = currentAnswer.text else { return }
         
+        if let solutionPostition = solutions.firstIndex(of: answerText) {
+            activatedButtons.removeAll()
+            
+            var splitAnswers = answerLabel.text?.components(separatedBy: "\n")
+            splitAnswers?[solutionPostition] = answerText
+            answerLabel.text = splitAnswers?.joined(separator: "\n")
+            
+            currentAnswer.text = ""
+            score += 1
+            completedAnswers += 1
+            
+            if completedAnswers == 7 && score != 0 {
+                let ac = UIAlertController(title: "Well Done", message: "Are you ready for the next level?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
+                present(ac, animated: true)
+            }
+        } else {
+            let ac = UIAlertController(title: "Oops!", message: "Sorry, wrong answer. Please try again!", preferredStyle: .alert)
+            let alert = UIAlertAction(title: "OK", style: .default, handler: nil)
+            ac.addAction(alert)
+            present(ac, animated: true)
+            
+            if score > 0 {
+                score -= 1
+            }
+        }
+        
+        clearTapped(clear)
     }
     
     @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
         
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+        
+        activatedButtons.removeAll()
     }
     
 }
